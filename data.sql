0ed1b40bdb2f8dddc010dcd8d5ce55c3a75bcd7e
Ronald <rjskinnerc@unal.edu.co>
2022-07-06 11:58:11 -0500

    Inser data for vets

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
+/* Project 1 */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES 
 ('Agumon', '2020-02-03', 0, true, 10.23),
@@ -7,6 +8,7 @@ VALUES
 ('Pikachu', '2021-01-07', 1, false, 15.04),
 ('Devimon', '2017-05-12', 5, true, 11);
 
+/* Project 2 */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
 ('Charmander', 'Feb 8, 2020', 0, false, -11),
@@ -72,6 +74,7 @@ SELECT * FROM animals;
 COMMIT TRANSACTION;
 SELECT * FROM animals;
 
+/* Project 3 */
 INSERT INTO owners (full_name, age)
 VALUES
 ('Sam Smith', 34),
@@ -114,3 +117,10 @@ UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
 WHERE name = 'Angemon' OR name = 'Boarmon';
 
+/* Project 4 */
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+('William Tatcher', 45, 'Apr 23, 2000'),
+('Maisy Smith', 26, 'Jan 17, 2019'),
+('Stephanie Mendez', 64, 'May 4, 1981'),
+('Jack Harkness', 38, 'Jun 8, 2008');
