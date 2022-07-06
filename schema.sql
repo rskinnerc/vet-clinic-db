50c1c160159b0e430cbbd8e0a2f1bf1d2d5f0986
Ronald <rjskinnerc@unal.edu.co>
2022-07-06 11:40:42 -0500

    Create vets table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Project 1 */
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
@@ -9,9 +10,11 @@ CREATE TABLE animals (
     weight_kg DECIMAL
 );
 
+/* Project 2 */
 ALTER TABLE animals
 ADD species VARCHAR(255);
 
+/* Project 3 */
 CREATE TABLE owners (
     id INT GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(255),
@@ -43,3 +46,12 @@ ADD CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
 
 ALTER TABLE animals
 ADD CONSTRAINT fk_owner_id FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+/* Project 4 */
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE
+);
+
