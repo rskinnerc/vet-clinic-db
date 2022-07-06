a9d3c5d437eaebf387663011cbe3b3327afae302
Ronald <rjskinnerc@unal.edu.co>
2022-07-05 21:53:51 -0500

    Assign owners to animals

--- data.sql
+++ data.sql
@@ -94,3 +94,23 @@ UPDATE animals
 SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
 WHERE species_id IS NULL;
 
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE name = 'Gabumon' OR name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name = 'Devimon' OR name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name = 'Angemon' OR name = 'Boarmon';
+
