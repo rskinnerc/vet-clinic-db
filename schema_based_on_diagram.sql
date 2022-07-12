010c3787eb88dc463997709b26c219f3851a4ba9
unknown <skj99667788@gmail.com>
2022-07-13 00:40:15 +0500

    invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,10 +6,21 @@ create table patients (
 );
 
 CREATE TABLE medical_histories (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
     admitted_at timestamp,
     patient_id INT,
     status varchar(255),
     PRIMARY KEY (id),
-    CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patients(id)
+    CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patients(id),
+
+);
+
+create table invoices (
+    id INT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id int,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
