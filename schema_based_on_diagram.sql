d76a09577239ac4f52b5bbc45891f5a54f2fdc02
Ronald <rjskinnerc@unal.edu.co>
2022-07-12 14:34:01 -0500

    Add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,12 @@ create table patients (
     date_of_birth date,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    patient_id INT,
+    status varchar(255),
+    PRIMARY KEY (id),
+    CONSTRAINT fk_patient FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
